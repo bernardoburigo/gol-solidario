213b2f873ee4590567702fb1d6e16a329c7eb679
bernardoburigo <bernardoburigo@hotmail.com>
2026-02-10 22:35:29 -0300

    feat: adicionando cores e novo card

---
+++ src/Components/TotalGeralCard.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  total: number;
+}
+
+export function TotalGeralCard({ total }: Props) {
+  return (
+    <div className="bg-green-900 text-yellow-300 rounded-2xl shadow-xl p-8 text-center max-w-xl mx-auto">
+      <h2 className="text-lg font-semibold mb-2">
+        💰 Total Arrecadado Geral
+      </h2>
+
+      <p className="text-4xl font-bold">
+        R$ {total.toFixed(2)}
+      </p>
+    </div>
+  );
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,27 +1,42 @@
 import { jogadores } from "../Data/jogadoresData";
 import { RankingCard } from "../Components/RankingCard";
 import { SwipeContainer } from "../Components/SwipeContainer";
+import { TotalGeralCard } from "../Components/TotalGeralCard";
+import { calcularValorTotal } from "../Utils/valorPorGolUtil";
 
 export function Home() {
+  const totalGeral = jogadores.reduce((acc, jogador) => {
+    const totalJogador = calcularValorTotal(
+      jogador.golsNormalTotal,
+      jogador.golsDemoTotal
+    );
+
+    return acc + totalJogador;
+  }, 0);
+
   return (
-    <div className="min-h-screen bg-(--color-bg-main) p-6">
-      <h1 className="text-3xl font-bold text-center text-(--color-primary-green) mb-8">
+    <div className="min-h-screen bg-yellow-400 p-6">
+      <h1 className="text-4xl font-bold text-center text-green-900 mb-8">
         ⚽ Gol Solidário
       </h1>
 
-      <SwipeContainer>
-        <RankingCard
-          titulo="Artilheiros do Mês"
-          jogadores={jogadores}
-          tipo="mes"
-        />
+      <TotalGeralCard total={totalGeral} />
 
-        <RankingCard
-          titulo="Artilheiros Geral"
-          jogadores={jogadores}
-          tipo="total"
-        />
-      </SwipeContainer>
+      <div className="mt-10">
+        <SwipeContainer>
+          <RankingCard
+            titulo="Artilheiros do Mês"
+            jogadores={jogadores}
+            tipo="mes"
+          />
+
+          <RankingCard
+            titulo="Artilheiros Geral"
+            jogadores={jogadores}
+            tipo="total"
+          />
+        </SwipeContainer>
+      </div>
     </div>
   );
 }
